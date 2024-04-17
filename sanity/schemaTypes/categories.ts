a6e289cd6cb808f998192929624805b42dace105
guille1999 <90706729+guille1999utp@users.noreply.github.com>
2024-04-17 18:32:57 -0500

    primera version de informacion con sanity

---
+++ sanity/schemaTypes/categories.ts
@@ -0,0 +1,32 @@
+export const category = [
+    {
+      title: "Categorias",
+      name: "category",
+      type: "document",
+      fields: [
+        {
+          title: "Nombre de la categoria",
+          name: "nombre",
+          type: "string"
+        },
+        {
+            title: "Imagen de marca",
+            name: "image_marca",
+            type: "image"
+        },
+        {
+            title: "Subcategorias",
+            name: "sub_category",
+            type: "array",
+            of: [
+              {
+                type: 'reference',
+                to: [
+                  {type: 'sub_category'}
+                ]
+              }
+            ]
+        }
+      ]
+    }  
+  ]
---
+++ sanity/schemaTypes/user.ts
@@ -0,0 +1,24 @@
+export const user = [
+    {
+      title: "Usuarios",
+      name: "user",
+      type: "document",
+      fields: [
+        {
+          title: "Nombre",
+          name: "nombre",
+          type: "string"
+        },
+        {
+            title: "Correo",
+            name: "correo",
+            type: "string"
+        },
+        {
+            title: "Contraseña",
+            name: "password",
+            type: "string"
+        }
+      ]
+    }  
+  ]
