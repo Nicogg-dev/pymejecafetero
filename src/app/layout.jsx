633b44f711b26a8f7ccbcc29b3193a51917c5bd9
Nicolas Giraldo Arenas <nicolas.giraldo1@utp.edu.co>
2024-04-19 23:35:05 -0500

    tamano pantalla

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
 
         <ShoppingCartProvider>
 
-          <div className="flex flex-col min-h-screen">
+          <div className="flex flex-col min-h-screen w-800 m-auto"  style={{maxWidth: "1300px"}}>
             <Nav />
             {children}
             <Footer />
